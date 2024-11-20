13cb510e9b42992b7fa245173418eae621d49dc9
Xuxin ZHU <81241980+Xuxin2333@users.noreply.github.com>
2024-11-20 17:56:36 +0100

    Update CalculatorService.js

--- CalculatorService.js
+++ CalculatorService.js
@@ -1,7 +1,7 @@
 var http = require ('http');
 var request = require('sync-request');
 
-const PORT = 80;
+const PORT = 31000;
 const service_ip = '192.168.189.67';
 
 const SUM_SERVICE_IP_PORT = 'http://'+service_ip+':31001';
